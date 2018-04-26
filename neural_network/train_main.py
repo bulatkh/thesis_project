d7bf146af74f90d3a5bee01ae3722188140641ee
bulatkh <bulatkhaert@gmail.com>
2018-04-26 23:05:41 +0300

    Initialized classes ImageLabel

--- neural_network/train_main.py
+++ neural_network/train_main.py
@@ -1,17 +1,20 @@
 from numpy import array
-from neural_network import model, label_images
+from neural_network import model
+from neural_network import label_images as li
 
 train_path = 'C:\\Users\\User\\Desktop\\\\Thesis\\data\\train\\'
 test_path = 'C:\\Users\\User\\Desktop\\\\Thesis\\data\\test\\'
 val_path = 'C:\\Users\\User\\Desktop\\\\Thesis\\data\\val\\'
 print("-")
-train_images, train_labels = label_images.read_images_with_labels(train_path)
+train_images_label = li.ImageLabel(train_path)
+train_images, train_labels = train_images_label.read_images_with_labels()
 print("-")
-test_images, test_labels = label_images.read_images_with_labels(test_path)
+test_images_label = li.ImageLabel(test_path)
+test_images, test_labels = test_images_label.read_images_with_labels()
 print("-")
-val_images, val_labels = label_images.read_images_with_labels(val_path)
+val_images_label = li.ImageLabel(val_path)
+val_images, val_labels = val_images_label.read_images_with_labels()
 print("--")
-
 
 test_labels = array(test_labels)
 train_labels = array(train_labels)
@@ -20,20 +23,9 @@ train_images = array(train_images)
 val_images = array(val_images)
 val_labels = array(val_labels)
 
-# 1. Зашумить изображения - расширить датасет
-# 2. Шаг обучения - придумать, как должен меняться
-# 3. Найти наилучшую архитектуру сети (количество слоев, размеры сверточных окон, размеры изоб.)
-# 4. Допилить во входные параметры алгоритм оптимизации, размер батча, размер полносвязного слоя)
-
-# first_batch_lab = test_labels[:32]
-# first_batch_img = test_images[:32]
-# batched_train_img, batched_train_lab = prepare.batch_dataset(train_images, train_labels)
-# batched_test_img, batched_test_lab = prepare.batch_dataset(test_images, test_labels)
-# print(train_labels)
-# batched_train_img = array(batched_train_img).reshape(32, 28, 28, 1)
-# batched_test_img = array(batched_test_img).reshape(32, 28, 28, 1)
 print("Training started!")
-model.create_nn_and_train(56, 32, 5, 2, 0.001, train_images, train_labels, test_images, test_labels, val_images, val_labels)
+model.create_nn_and_train(56, 32, 5, 2, 0.001, train_images, train_labels, test_images, test_labels, val_images,
+                          val_labels)
 
 
 
