1aa87ed6cf16cf241b5410045ece5c99a3bd9ea5
bulatkh <bulatkhaert@gmail.com>
2018-04-26 23:16:10 +0300

    Added class ImageConverter

--- preprocess/convert_image.py
+++ preprocess/convert_image.py
@@ -1,35 +1,34 @@
 from PIL import Image
 import os
 
-size = 56, 56
 
+class ImageConverter(object):
 
-def rgb2binar(path):
-    files = os.listdir(path)
-    files = list(filter(lambda x: ".png" in x, files))
-    length = len(files)
-    counter = 0
-    for file in files:
-        counter += 1
-        if counter % 1000 == 0:
-            print("Binarization progress: " + str(counter / length * 100) + " %")
-        img = Image.open(path + file, 'r')
-        bg = Image.new("RGB", (278, 278), (255, 255, 255))
-        bg.paste(img, (0, 0), mask=img)
-        gray = bg.convert('L')
-        binary = gray.point(lambda x: 0 if x < 128 else 255, "1")
-        binary.save(path + file)
+    def __init__(self, path):
+        self.path = path
+        self.files = list(filter(lambda x: ".png" in x, os.listdir(path)))
 
+    def rgb2binar_alpha_png(self):
+        length = len(self.files)
+        counter = 0
+        for file in self.files:
+            counter += 1
+            if counter % 1000 == 0:
+                print("Binarization progress: " + str(counter / length * 100) + " %")
+            img = Image.open(self.path + file, 'r')
+            bg = Image.new("RGB", (278, 278), (255, 255, 255))
+            bg.paste(img, (0, 0), mask=img)
+            gray = bg.convert('L')
+            binary = gray.point(lambda x: 0 if x < 128 else 255, "1")
+            binary.save(self.path + file)
 
-def resize(path, size):
-    files = os.listdir(path)
-    files = list(filter(lambda x: ".png" in x, files))
-    length = len(files)
-    counter = 0
-    for file in files:
-        counter += 1
-        if counter % 1000 == 0:
-            print("Resizing progress: " + str(counter / length * 100) + " %")
-        img = Image.open(path + file, 'r')
-        img.thumbnail(size, Image.ANTIALIAS)
-        img.save(path + file)
+    def resize(self, size):
+        length = len(self.files)
+        counter = 0
+        for file in self.files:
+            counter += 1
+            if counter % 1000 == 0:
+                print("Resizing progress: " + str(counter / length * 100) + " %")
+            img = Image.open(self.path + file, 'r')
+            img.thumbnail(size, Image.ANTIALIAS)
+            img.save(self.path + file)
