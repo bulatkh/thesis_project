f6ad8c3ae222fc7a4e2074f9eee1e442cf632a5e
bulatkh <bulatkhaert@gmail.com>
2018-05-09 13:15:21 +0300

    Added encapsulation using @property Return separately first and second names

--- text_recognition/FormParser.py
+++ text_recognition/FormParser.py
@@ -5,36 +5,69 @@ import os
 
 class FormParser(object):
     def __init__(self, filename):
-        self.filename = filename
+        self._filename = filename
+        self._second_name_path = ''
+        self._name_path = ''
+
+    @property
+    def filename(self):
+        return self._filename
+
+    @property
+    def second_name_path(self):
+        return self._second_name_path
+
+    @second_name_path.setter
+    def second_name_path(self, path):
+        self._second_name_path = path
+
+    @property
+    def name_path(self):
+        return self._name_path
+
+    @name_path.setter
+    def name_path(self, path):
+        self._name_path = path
 
     def get_chars(self):
         img = Image.open(self.filename, 'r')
         os.mkdir(self.filename[:-4])
         images = []
-        images.append(img.crop((134, 221, 1596, 300)))
-        images.append(img.crop((144, 465, 1581, 540)))
+        images.append(img.crop((147, 224, 1610, 300)))
+        images.append(img.crop((162, 465, 1590, 545)))
         counter = 0
+        self.second_name_path = self.filename[:-4] + '\\second_name\\'
+        self.name_path = self.filename[:-4] + '\\name\\'
+        os.mkdir(self.second_name_path)
+        os.mkdir(self.name_path)
         for img in images:
             img.show()
             width, height = img.size
             one_width = round(width / 20)
             for i in range(0, width - one_width, one_width):
-                    img.crop((i, 0, i + one_width, height)).save(self.filename[:-4] + '\\' + str(counter) + '.jpg')
+                    if counter < 20:
+                        img.crop((i, 0, i + one_width, height)).save(
+                            self.second_name_path + str(counter) + '.jpg')
+                    else:
+                        img.crop((i, 0, i + one_width, height)).save(
+                            self.name_path + str(counter) + '.jpg')
                     counter += 1
 
-    def cut_borders(self):
-        files = os.listdir(self.filename[:-4])
+    @staticmethod
+    def cut_borders(path):
+        files = os.listdir(path)
         files = list(filter(lambda x: '.jpg' in x, files))
         for file in files:
-            img = Image.open(self.filename[:-4] + '\\' + file, 'r')
+            img = Image.open(path + file, 'r')
             width, height = img.size
-            img.crop((7, 7, width - 7, height - 7)).save(self.filename[:-4] + '\\' + file)
+            img.crop((7, 7, width - 7, height - 7)).save(path + '\\' + file)
 
-    def delete_empty(self):
-        files = os.listdir(self.filename[:-4])
+    @staticmethod
+    def delete_empty(path):
+        files = os.listdir(path)
         files = list(filter(lambda x: '.jpg' in x, files))
         for file in files:
-            tmp_img = Image.open(self.filename[:-4] + '\\' + file, 'r')
+            tmp_img = Image.open(path + '\\' + file, 'r')
             tmp_img = tmp_img.convert('L')
             binary = tmp_img.point(lambda pix: 0 if pix < 128 else 255, "1")
             pixels = list(binary.getdata())
@@ -43,21 +76,27 @@ class FormParser(object):
                 if pixel == 0:
                     sum += 1
             if sum / len(pixels) < 0.05:
-                os.remove(self.filename[:-4] + '\\' + file)
+                os.remove(path + '\\' + file)
 
-    def recognize_chars(self, nn_path):
-        files = os.listdir(self.filename[:-4])
+    @staticmethod
+    def recognize_chars(path, nn_path):
+        files = os.listdir(path)
         files = list(filter(lambda x: '.jpg' in x, files))
         files = [int(file[:-4]) for file in files]
         files.sort()
-        predictor = Predictor.Predictor(self.filename[:-4], nn_path)
+        predictor = Predictor.Predictor(path, nn_path)
         final_answer = []
         for file in files:
             file = str(file) + '.jpg'
-            _, prediction = predictor.make_prediction(self.filename[:-4] + '\\' + file, nn_path)
+            _, prediction = predictor.make_prediction(path + '\\' + file, nn_path)
             recognized_char, _ = predictor.analyze_prediction(prediction)
             final_answer.append(recognized_char)
         return ''.join(final_answer)
+
+    def get_full_name(self, nn_path):
+        second_name = FormParser.recognize_chars(self._second_name_path, nn_path)
+        first_name = FormParser.recognize_chars(self._name_path, nn_path)
+        return first_name, second_name
 
 
 if __name__ == '__main__':
@@ -65,7 +104,9 @@ if __name__ == '__main__':
     nn_path = 'C:\\Users\\User\\Desktop\\Thesis\\trained_nn\\kaggle\\bigbatch.ckpt-4126'
     parser = FormParser(form_path)
     parser.get_chars()
-    parser.cut_borders()
-    parser.delete_empty()
-    answer = parser.recognize_chars(nn_path)
-    print(answer)
+    parser.cut_borders(parser.name_path)
+    parser.cut_borders(parser.second_name_path)
+    parser.delete_empty(parser.name_path)
+    parser.delete_empty(parser.second_name_path)
+    first, second = parser.get_full_name(nn_path)
+    print(first + ' ' + second)
